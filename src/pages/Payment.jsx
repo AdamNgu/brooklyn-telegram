83385c26f2bffe3522290074cc59eccce892e5bd
Tomasz Kawik <tomasz.kawik@debijenkorf.nl>
2019-01-05 15:28:55 +0100

    Setup routing.

---
+++ src/pages/Payment.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Payment = () => {
+    return (
+        <div>
+            Here will be fancy PAYMENT stuff
+        </div>
+    );
+};
+
+Payment.displayName = 'Payment';
+
+export default Payment;
